38596dc8927f7b74d010619d34058eef739296eb
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2017-12-23 17:50:37 -0800

    Fix endDurationAhead indicator in server output.

--- fow-server/progress_handler.go
+++ fow-server/progress_handler.go
@@ -120,7 +120,7 @@ func progressHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprint(w, formatOutput(0, progress, 0), ":")
 	}
-	fmt.Fprint(w, config.updateFrequency)
+	fmt.Fprint(w, config.updateFrequency*1000) // 1000 converts to milliseconds, we don't use time constants because they are arbitrary
 	data.updateMux.Unlock()
 }
 
